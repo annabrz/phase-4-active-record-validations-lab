c2eb748c61ca5d87a85945c721b51e1b23365f4b
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-11-26 22:46:15 -0800

    commit tree b51164c7a19c53a89d6dc8f791849ad2881762e7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :post_validator
+
+
+
+  def post_validator
+    if title == "True Facts"
+      errors.add(:title, "Must include more clickbait!")
+    end
+  end
 end
